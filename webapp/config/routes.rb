21074ee00178c8fd9f21a6c2bd897c0f5594c207
laomatt <laomatt1@gmail.com>
2016-11-09 18:38:28 -0800

    Create users decks and cards

--- webapp/config/routes.rb
+++ webapp/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :cards
+  resources :decks
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
