94618554cf137d0fddd6ffb37793252843c93006
laomatt <laomatt1@gmail.com>
2016-11-09 18:50:52 -0800

    Create initial schema

--- webapp/db/migrate/20161110023814_create_decks.rb
+++ webapp/db/migrate/20161110023814_create_decks.rb
@@ -1,6 +1,9 @@
 class CreateDecks < ActiveRecord::Migration
   def change
     create_table :decks do |t|
+      t.integer :user_id
+      t.string :name
+      t.text :desc
 
       t.timestamps null: false
     end
--- webapp/db/migrate/20161110023818_create_cards.rb
+++ webapp/db/migrate/20161110023818_create_cards.rb
@@ -1,6 +1,12 @@
 class CreateCards < ActiveRecord::Migration
   def change
     create_table :cards do |t|
+      t.integer :deck_id
+      t.string :name
+      t.string :question
+      t.string :answer
+      t.text :desc
+      t.boolean :shared
 
       t.timestamps null: false
     end
